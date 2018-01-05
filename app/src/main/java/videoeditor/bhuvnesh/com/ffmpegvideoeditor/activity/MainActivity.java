1e30010c5a85f9b33ade54ffc96f29a9cdd0b6c9
Branden Archer <b.m.archer4@gmail.com>
2018-01-04 23:28:04 -0500

    Use a more app centric TAG

--- app/src/main/java/videoeditor/bhuvnesh/com/ffmpegvideoeditor/activity/MainActivity.java
+++ app/src/main/java/videoeditor/bhuvnesh/com/ffmpegvideoeditor/activity/MainActivity.java
@@ -58,7 +58,7 @@ public class MainActivity extends AppCompatActivity {
     private FFmpeg ffmpeg;
     private ProgressDialog progressDialog;
     private Uri selectedVideoUri;
-    private static final String TAG = "BHUVNESH";
+    private static final String TAG = "VideoEditor";
     private static final String FILEPATH = "filepath";
     private int choice = 0;
     private int stopPosition;
