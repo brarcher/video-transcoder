4644cc2c426f98ea6928cb80633ad24613f9582d
Branden Archer <b.m.archer4@gmail.com>
2018-01-05 18:20:05 -0500

    Add and Abount dialog

--- app/src/main/java/videoeditor/bhuvnesh/com/ffmpegvideoeditor/activity/MainActivity.java
+++ app/src/main/java/videoeditor/bhuvnesh/com/ffmpegvideoeditor/activity/MainActivity.java
@@ -7,6 +7,7 @@ import android.content.ContentUris;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.graphics.drawable.ColorDrawable;
@@ -23,9 +24,12 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
+import android.webkit.WebView;
 import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -36,6 +40,7 @@ import com.github.hiteshsondhi88.libffmpeg.FFmpeg;
 import com.github.hiteshsondhi88.libffmpeg.LoadBinaryResponseHandler;
 import com.github.hiteshsondhi88.libffmpeg.exceptions.FFmpegCommandAlreadyRunningException;
 import com.github.hiteshsondhi88.libffmpeg.exceptions.FFmpegNotSupportedException;
+import com.google.common.collect.ImmutableMap;
 
 import org.apache.commons.io.comparator.LastModifiedFileComparator;
 import org.florescu.android.rangeseekbar.RangeSeekBar;
@@ -43,7 +48,9 @@ import org.florescu.android.rangeseekbar.RangeSeekBar;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Calendar;
 import java.util.List;
+import java.util.Map;
 
 import videoeditor.bhuvnesh.com.ffmpegvideoeditor.R;
 
@@ -988,6 +995,111 @@ public class MainActivity extends AppCompatActivity {
         textDialog.setContentView(R.layout.dialog_singleoption_text);
         textDialog.setCancelable(false);
         return textDialog;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        int id = item.getItemId();
+
+        if(id == R.id.action_about)
+        {
+            displayAboutDialog();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void displayAboutDialog()
+    {
+        final Map<String, String> USED_LIBRARIES = ImmutableMap.of
+        (
+            "Commons IO", "https://commons.apache.org/proper/commons-io/",
+            "FFmpeg", "https://www.ffmpeg.org/",
+            "FFmpeg Android", "http://writingminds.github.io/ffmpeg-android/",
+            "Guava", "https://github.com/google/guava",
+            "Range SeekBar", "https://github.com/anothem/android-range-seek-bar"
+        );
+
+        final Map<String, String> USED_ASSETS = ImmutableMap.of
+        (
+            "Film by Mint Shirt", "https://thenounproject.com/term/film/395618/"
+        );
+
+        StringBuilder libs = new StringBuilder().append("<ul>");
+        for (Map.Entry<String, String> entry : USED_LIBRARIES.entrySet())
+        {
+            libs.append("<li><a href=\"").append(entry.getValue()).append("\">").append(entry.getKey()).append("</a></li>");
+        }
+        libs.append("</ul>");
+
+        StringBuilder resources = new StringBuilder().append("<ul>");
+        for (Map.Entry<String, String> entry : USED_ASSETS.entrySet())
+        {
+            resources.append("<li><a href=\"").append(entry.getValue()).append("\">").append(entry.getKey()).append("</a></li>");
+        }
+        resources.append("</ul>");
+
+        String appName = getString(R.string.app_name);
+        int year = Calendar.getInstance().get(Calendar.YEAR);
+
+        String version = "?";
+        try
+        {
+            PackageInfo pi = getPackageManager().getPackageInfo(getPackageName(), 0);
+            version = pi.versionName;
+        }
+        catch (PackageManager.NameNotFoundException e)
+        {
+            Log.w(TAG, "Package name not found", e);
+        }
+
+        WebView wv = new WebView(this);
+        String html =
+            "<meta http-equiv=\"content-type\" content=\"text/html; charset=utf-8\" />" +
+            "<h1>" +
+            String.format(getString(R.string.about_title_fmt),
+                    "<a href=\"" + getString(R.string.app_webpage_url)) + "\">" +
+            appName +
+            "</a>" +
+            "</h1><p>" +
+            appName +
+            " " +
+            String.format(getString(R.string.debug_version_fmt), version) +
+            "</p><p>" +
+            String.format(getString(R.string.app_revision_fmt),
+                    "<a href=\"" + getString(R.string.app_revision_url) + "\">" +
+                            getString(R.string.app_revision_url) +
+                            "</a>") +
+            "</p><hr/><p>" +
+            String.format(getString(R.string.app_copyright_fmt), year) +
+            "</p><hr/><p>" +
+            getString(R.string.app_license) +
+            "</p><hr/><p>" +
+            String.format(getString(R.string.app_libraries), appName, libs.toString()) +
+            "</p><hr/><p>" +
+            String.format(getString(R.string.app_resources), appName, resources.toString());
+
+        wv.loadDataWithBaseURL("file:///android_res/drawable/", html, "text/html", "utf-8", null);
+        new AlertDialog.Builder(this)
+                .setView(wv)
+                .setCancelable(true)
+                .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener()
+                {
+                    public void onClick(DialogInterface dialog, int which)
+                    {
+                        dialog.dismiss();
+                    }
+                })
+                .show();
     }
 
 }
