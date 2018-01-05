5188a16724dd72f62f786a85c69ed6e6dff8483e
Branden Archer <b.m.archer4@gmail.com>
2018-01-04 21:14:25 -0500

    Avoid null pointer dereference if ActionBar does not exist

--- app/src/main/java/videoeditor/bhuvnesh/com/ffmpegvideoeditor/activity/PreviewImageActivity.java
+++ app/src/main/java/videoeditor/bhuvnesh/com/ffmpegvideoeditor/activity/PreviewImageActivity.java
@@ -2,6 +2,7 @@ package videoeditor.bhuvnesh.com.ffmpegvideoeditor.activity;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -25,8 +26,13 @@ public class PreviewImageActivity extends AppCompatActivity {
         protected void onCreate(@Nullable Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_gallery);
-            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-            getSupportActionBar().setDisplayShowHomeEnabled(true);
+            ActionBar actionBar = getSupportActionBar();
+            if(actionBar != null)
+            {
+                actionBar.setDisplayHomeAsUpEnabled(true);
+                actionBar.setDisplayShowHomeEnabled(true);
+            }
+
             TextView tvInstruction=(TextView)findViewById(R.id.tvInstruction) ;
 
             GridLayoutManager lLayoutlLayout = new GridLayoutManager(PreviewImageActivity.this, 4);
