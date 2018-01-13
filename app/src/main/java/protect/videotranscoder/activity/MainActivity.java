ca279e6d637667f1e196f5821c4e2e4717040221
Branden Archer <b.m.archer4@gmail.com>
2018-01-12 22:34:19 -0500

    Remove unused import

--- app/src/main/java/protect/videotranscoder/activity/MainActivity.java
+++ app/src/main/java/protect/videotranscoder/activity/MainActivity.java
@@ -27,7 +27,6 @@ import android.webkit.WebView;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
-import android.widget.SpinnerAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.VideoView;
