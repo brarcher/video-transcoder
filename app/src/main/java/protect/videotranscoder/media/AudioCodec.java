580a18a6f42245c46420ad7e61c28ca800c88a96
Branden Archer <b.m.archer4@gmail.com>
2018-01-12 23:01:56 -0500

    Add supported channels data to AudioCodec
    The vorbis codec only supports stereo with ffmpeg.

--- app/src/main/java/protect/videotranscoder/media/AudioCodec.java
+++ app/src/main/java/protect/videotranscoder/media/AudioCodec.java
@@ -2,21 +2,27 @@ package protect.videotranscoder.media;
 
 import android.support.annotation.Nullable;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * List of audio codecs which the application may support
  */
 public enum AudioCodec
 {
-    AAC("aac"),
-    MP3("mp3"),
-    VORBIS("vorbis"),
+    AAC("aac", Arrays.asList("1", "2")),
+    MP3("mp3", Arrays.asList("1", "2")),
+    VORBIS("vorbis", Collections.singletonList("2")),
     ;
 
     public final String ffmpegName;
+    public final List<String> supportedChannels;
 
-    AudioCodec(String ffmpegName)
+    AudioCodec(String ffmpegName, List<String> supportedChannels)
     {
         this.ffmpegName = ffmpegName;
+        this.supportedChannels = Collections.unmodifiableList(supportedChannels);
     }
 
     @Nullable
