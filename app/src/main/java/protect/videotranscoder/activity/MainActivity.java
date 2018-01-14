f294f1646b5a9981d26c75d4e4dc5e603ba4ba41
Branden Archer <b.m.archer4@gmail.com>
2018-01-13 23:17:50 -0500

    Move Uri resolving methods to separate class

--- app/src/main/java/protect/videotranscoder/activity/MainActivity.java
+++ app/src/main/java/protect/videotranscoder/activity/MainActivity.java
@@ -52,6 +52,7 @@ import java.util.TimerTask;
 
 import protect.videotranscoder.FFmpegResponseHandler;
 import protect.videotranscoder.FFmpegUtil;
+import protect.videotranscoder.FileUtil;
 import protect.videotranscoder.R;
 import protect.videotranscoder.ResultCallbackHandler;
 import protect.videotranscoder.media.AudioCodec;
@@ -659,7 +660,7 @@ public class MainActivity extends AppCompatActivity
             if (requestCode == REQUEST_TAKE_GALLERY_VIDEO)
             {
                 Uri selectedVideoUri = data.getData();
-                String videoPath = getPath(MainActivity.this, selectedVideoUri);
+                String videoPath = FileUtil.getPath(MainActivity.this, selectedVideoUri);
 
                 if(videoPath == null)
                 {
@@ -793,144 +794,6 @@ public class MainActivity extends AppCompatActivity
             progressBar.setVisibility(View.GONE);
         }
     };
-
-    /**
-     * Get a file path from a Uri. This will get the the path for Storage Access
-     * Framework Documents, as well as the _data field for the MediaStore and
-     * other file-based ContentProviders.
-     */
-    private String getPath(final Context context, final Uri uri)
-    {
-        // DocumentProvider
-        if (DocumentsContract.isDocumentUri(context, uri))
-        {
-            // ExternalStorageProvider
-            if (isExternalStorageDocument(uri))
-            {
-                final String docId = DocumentsContract.getDocumentId(uri);
-                final String[] split = docId.split(":");
-                final String type = split[0];
-
-                if ("primary".equalsIgnoreCase(type))
-                {
-                    return Environment.getExternalStorageDirectory() + "/" + split[1];
-                }
-
-                // TODO handle non-primary volumes
-            }
-            // DownloadsProvider
-            else if (isDownloadsDocument(uri))
-            {
-                final String id = DocumentsContract.getDocumentId(uri);
-                final Uri contentUri = ContentUris.withAppendedId(
-                        Uri.parse("content://downloads/public_downloads"), Long.parseLong(id));
-
-                return getDataColumn(context, contentUri, null, null);
-            }
-            // MediaProvider
-            else if (isMediaDocument(uri))
-            {
-                final String docId = DocumentsContract.getDocumentId(uri);
-                final String[] split = docId.split(":");
-                final String type = split[0];
-
-                Uri contentUri = null;
-                if ("image".equals(type))
-                {
-                    contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
-                }
-                else if ("video".equals(type))
-                {
-                    contentUri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
-                }
-                else if ("audio".equals(type))
-                {
-                    contentUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
-                }
-
-                final String selection = "_id=?";
-                final String[] selectionArgs = new String[]
-                {
-                    split[1]
-                };
-
-                return getDataColumn(context, contentUri, selection, selectionArgs);
-            }
-        }
-        // MediaStore (and general)
-        else if ("content".equalsIgnoreCase(uri.getScheme()))
-        {
-            return getDataColumn(context, uri, null, null);
-        }
-        // File
-        else if ("file".equalsIgnoreCase(uri.getScheme()))
-        {
-            return uri.getPath();
-        }
-
-        return null;
-    }
-
-    /**
-     * Get the value of the data column for this Uri.
-     */
-    private String getDataColumn(Context context, Uri uri, String selection,
-                                 String[] selectionArgs)
-    {
-        Cursor cursor = null;
-        final String column = "_data";
-        final String[] projection =
-        {
-            column
-        };
-
-        try
-        {
-            cursor = context.getContentResolver().query(uri, projection, selection, selectionArgs,
-                    null);
-            if (cursor != null && cursor.moveToFirst())
-            {
-                final int column_index = cursor.getColumnIndexOrThrow(column);
-                return cursor.getString(column_index);
-            }
-        }
-        finally
-        {
-            if (cursor != null)
-            {
-                cursor.close();
-            }
-        }
-        return null;
-    }
-
-
-    /**
-     * @param uri The Uri to check.
-     * @return Whether the Uri authority is ExternalStorageProvider.
-     */
-    private boolean isExternalStorageDocument(Uri uri)
-    {
-        return "com.android.externalstorage.documents".equals(uri.getAuthority());
-    }
-
-    /**
-     * @param uri The Uri to check.
-     * @return Whether the Uri authority is DownloadsProvider.
-     */
-    private boolean isDownloadsDocument(Uri uri)
-    {
-        return "com.android.providers.downloads.documents".equals(uri.getAuthority());
-    }
-
-    /**
-     * @param uri The Uri to check.
-     * @return Whether the Uri authority is MediaProvider.
-     */
-    private boolean isMediaDocument(Uri uri)
-    {
-        return "com.android.providers.media.documents".equals(uri.getAuthority());
-    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu)
