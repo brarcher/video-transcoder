fcd0d7e95962f08d5a7597001a1641bb155d56d4
Branden Archer <b.m.archer4@gmail.com>
2018-06-24 23:26:42 -0400

    Add setting for file search location
    When open the file browser to find a file, the starting location
    used to be external storage. It was mentioned that the last used
    path would be easier, so this was instead made the default.
    
    It turns out that navigating to the internal storage device
    may prevent one from navigating back to external storage. The
    way to recover was to wipe the app's data, resetting its last
    used path. To avoid this, a new setting is added which sets what
    the start path should be. The default is to use the last used
    path, however it can be fixed to the external storage location.

---
+++ app/src/main/java/protect/videotranscoder/activity/SettingsActivity.java
@@ -0,0 +1,58 @@
+package protect.videotranscoder.activity;
+
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+import protect.videotranscoder.R;
+
+public class SettingsActivity extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings_layout);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if(actionBar != null)
+        {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+
+        // Display the fragment as the main content.
+        getFragmentManager().beginTransaction()
+                .replace(R.id.fragment_container, new SettingsFragment())
+                .commit();
+    }
+
+    public static class SettingsFragment extends PreferenceFragment
+    {
+        @Override
+        public void onCreate(Bundle savedInstanceState)
+        {
+            super.onCreate(savedInstanceState);
+
+            // Load the preferences from an XML resource
+            addPreferencesFromResource(R.xml.settings);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        int id = item.getItemId();
+
+        if(id == android.R.id.home)
+        {
+            finish();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
